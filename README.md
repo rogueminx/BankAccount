a6740827203e0decd35cc75d6b8f42755c041905
Ben Parisot <me@benparisot.com>
2017-11-21 18:10:35 -0800

    create object and prototype

---
+++ README.md
@@ -0,0 +1,6 @@
+SPECS:
+1. take in name from user and store in account object
+2. take in initial deposit from user and set as initial account balance in account object
+3. take in additional deposit and add to initial account balance
+4. take in withdrawal and subtract from account balance
+5. print onscreen at any time (maybe always change, or with keystroke)
--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,24 @@
 //back end logic
-var balance = [];
+function Account(name, balance) {
+	this.name = name;
+	this.balance = balance;
+};
+
+Account.prototype.deposit = function() {
+
+};
 
 //user interface logic
 $(document).ready(function() {
-	$("form#input").submit(function(event) {
+	$("form#setup").submit(function(event) {
 		event.preventDefault();
-		var name = $("#name").val();
+		var inputtedName = $("#name").val();
 		var initialDeposit = parseInt($("#initial").val());
-		var mainDeposit = parseInt($("#main").val());
-		var withdrawal = parseInt($("#withdrawal").val());
-		balance.push(name, initialDeposit, mainDeposit, withdrawal);
-		console.log(balance);
+		var newAccount = new Account(inputtedName, initialDeposit);
+		// var mainDeposit = parseInt($("#main").val());
+		// var withdrawal = parseInt($("#withdrawal").val());
+		// balance.push(name, initialDeposit, mainDeposit, withdrawal);
+		console.log(newAccount);
 	});
 
 });
